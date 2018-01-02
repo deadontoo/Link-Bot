9c24e27b19d71c6a87884e160872d672c1282ca6
deadontoo <35027375+deadontoo@users.noreply.github.com>
2018-01-02 03:41:45 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+var bot = new Discord.Client();
+const config = require("./config.json");
+
+bot.on('ready', () => {
+    console.log('i am ready');
+});
+
+bot.on('message', message => {
+    if(message.author.bot) return;
+    var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
+    if(re != null){
+        message.delete().then(message => {
+            message.author.send('Sorry, you cannot include links in your messages');
+        });
+    }
+});
+
+bot.login('Mzk3NjY3ODUyMTc1NTQwMjM2.DSzZlA.XzBvA65VVC47JxxgGzyXlR5VdwM');
